52183175fc3af8be7b87d363f341c06892adec36
Nicholas <nicholasdalba@gmail.com>
2024-11-23 17:28:30 -0800

    Initial Commit

---
+++ Animated Set/Animated_SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_SetApp.swift
+//  Animated Set
+//
+//  Created by Nicholas Alba on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Animated_SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
